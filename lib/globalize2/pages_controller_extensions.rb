1cf5f1924251f1ba338819de85d01369acad9888
Benny Degezelle <benny@gorilla-webdesign.be>
2011-03-30 15:40:51 +0200

    fixing reset_locale on admin/pages/new

--- lib/globalize2/pages_controller_extensions.rb
+++ lib/globalize2/pages_controller_extensions.rb
@@ -5,5 +5,10 @@ module Globalize2
         before_filter :reset_locale, :only => [:new]
       end
     end
+    
+    def reset_locale
+      @locale = Globalize2Extension.default_language
+      Globalize2Extension.content_locale = I18n.locale = @locale.to_sym
+    end
   end
 end
