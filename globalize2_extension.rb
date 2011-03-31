bc1812601d5def9fe86e6b244c6f3b66f0c69b9c
Benny Degezelle <benny@gorilla-webdesign.be>
2011-03-31 13:22:12 +0200

    bumping version number

--- globalize2_extension.rb
+++ globalize2_extension.rb
@@ -3,7 +3,7 @@
 require 'globalize2/form_builder_extensions'
 
 class Globalize2Extension < Radiant::Extension
-  version "0.2.1"
+  version "0.2.2"
   description "Translate content in Radiant CMS using the Globalize2 Rails plugin."
   url "http://blog.aissac.ro/radiant/globalize2-extension/"
 
